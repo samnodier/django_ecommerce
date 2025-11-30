8e3cf247bfd810effae50bef5474d98fdef0ab9f
Sam Nodier <samnodier@Sams-MacBook-Air.local>
2025-11-30 03:01:12 +0200

    search ui

--- store/static/store/js/header.js
+++ store/static/store/js/header.js
@@ -1,4 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const searchButton = document.querySelector("#search-toggle");
+  if (!searchButton) return;
+  searchButton.addEventListener("click", toggleSearch);
+});
+
+document.addEventListener("click", (event) => {
+  // Close search when clicking outside
+  const searchContainer = document.getElementById("search-container");
+  const searchButton = event.target.closest("#search-toggle");
+  if (
+    !searchContainer.contains(event.target) &&
+    !searchButton &&
+    !searchContainer.classList.contains("hidden")
+  ) {
+    toggleSearch();
+  }
+});
+
 function toggleSearch() {
+  console.log("working");
   const searchContainer = document.getElementById("search-container");
   const searchInput = document.getElementById("search-input");
 
